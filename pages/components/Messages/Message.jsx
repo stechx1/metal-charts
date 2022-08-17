6ce93d7670ee7a7a0971b4dd7fde2687b12e4eaf
techx1 <ss.uitech01@gmail.com>
2022-08-18 03:43:27 +0500

    scroll into view implemented

--- pages/components/Messages/Message.jsx
+++ pages/components/Messages/Message.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
 
 const Message = ({ senderName, receiverName, message, own }) => {
+  const myRef = useRef(null);
+   useEffect(() => {
+      myRef.current.scrollIntoView();
+    }, [message]);
   return (
     <div className="chat-message">
       <div className={`flex items-end ${own ? "justify-end" : ""}`}>
@@ -11,7 +15,7 @@ const Message = ({ senderName, receiverName, message, own }) => {
         >
           <span className="font-bold">{own ? senderName : receiverName}</span>
           <div>
-            <span
+            <span ref={myRef}
               className={`inline-block rounded-lg rounded-br-none px-4 py-2 ${
                 own ? "bg-blue-600" : "bg-gray-300 text-gray-600"
               } text-white`}
